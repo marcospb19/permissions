95ab5cedf0a4145e1cd945128368c540752650c1
João M. Bezerra <marcospb19@hotmail.com>
2020-09-21 03:15:55 -0300

    Addapted access function to receive our ModeBits

--- src/functions.rs
+++ src/functions.rs
@@ -1,3 +1,5 @@
+use crate::ModeBits;
+
 use std::{ffi::CString, io, path::Path};
 
 /// Check if current process has permission to remove file/directory at
@@ -18,27 +20,56 @@ pub fn is_file_removable(path: &impl AsRef<Path>) -> io::Result<bool> {
         Some(parent) => parent,
     };
 
-    access(&parent, libc::W_OK)
+    access(&parent, ModeBits::WRITE)
 }
 
 /// Check if current process has permission to read file/directory at path.
 pub fn is_file_readable(path: &impl AsRef<Path>) -> io::Result<bool> {
-    access(&path.as_ref(), libc::R_OK)
+    access(&path.as_ref(), ModeBits::READ)
 }
 
 /// Check if current process has permission to write to file/directory at path.
 pub fn is_file_writable(path: &impl AsRef<Path>) -> io::Result<bool> {
-    access(&path.as_ref(), libc::W_OK)
+    access(&path.as_ref(), ModeBits::WRITE)
 }
 
 /// Check if current process has permission to execute file/directory at path.
 pub fn is_file_executable(path: &impl AsRef<Path>) -> io::Result<bool> {
-    access(&path.as_ref(), libc::X_OK)
+    access(&path.as_ref(), ModeBits::EXECUTE)
 }
 
-/// Safe function that wraps `libc::access` syscall, used by other functions in
-/// the same module.
-pub fn access(path: &impl AsRef<Path>, modes: libc::c_int) -> io::Result<bool> {
+/// Safe function that wraps `libc::access` syscall.
+///
+/// `mode: ModeBits` can be a mask consisting of the bitwise _OR_ of one or more
+/// of `R_OK`, `W_OK`, and `X_OK`.
+///
+/// If `mode` is an empty mask, this function passes `F_OK` and only checks if
+/// the file exists.
+///
+/// References:
+/// `man access`,
+/// `man 2 access`, or
+/// [online man page](https://man7.org/linux/man-pages/man2/access.2.html)
+pub fn access(path: &impl AsRef<Path>, modes: ModeBits) -> io::Result<bool> {
+    // Let's translate ModeBits to the c_int
+    let mut temp = 0;
+    if modes.is_read_set() {
+        temp |= libc::R_OK;
+    }
+    if modes.is_write_set() {
+        temp |= libc::W_OK;
+    }
+    if modes.is_execute_set() {
+        temp |= libc::X_OK;
+    }
+    // If nothing was set before, just check if the file exists with F_OK
+    // See references at function doc for more explanation
+    if temp == 0 {
+        temp |= libc::F_OK;
+    }
+
+    let modes = temp;
+
     let bytes: Vec<u8> = path.as_ref().to_str().unwrap().bytes().collect();
     let cstring = CString::new(bytes).unwrap();
     let result = unsafe { libc::access(cstring.as_ptr(), modes) };
