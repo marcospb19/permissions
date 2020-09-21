26109b1e2cf899fe471f6e464c22929959e478d6
João M. Bezerra <marcospb19@hotmail.com>
2020-09-21 15:54:11 -0300

    Left to elaborate example1.rs, this commits will be amended

--- examples/example1.rs
+++ examples/example1.rs
@@ -1,24 +1,4 @@
-// This is the basic API we are trying to achieve!
-// Simbora cara, vai dar certo
-
-// Tem mais coisa além disso aqui, na verdade, aqui tá o básico da ideia
-
-/*
-Possible changes here
-
-Should I change:
-
-ModeBits to Bits?
-ModeBits::AllSet to {AllModes or AllBits}?
-Classes::AllClasses to {AllClasses or Any or All}?
-*/
-
-// Importando os cabra bom tudo exibido
-use unix_file_permissions::{
-    Classes::{AllClasses, Group, Other, Owner},
-    ModeBits::{AllSet, Execute, Null, Read, Write},
-    PermissionBits,
-};
+use unix_file_permissions::{Classes, ModeBits, PermissionBits};
 
 // use std::fs::File;
 
