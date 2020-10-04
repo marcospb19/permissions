989cd775b3eb8050f8fec3ca07414d9d2a7e8f76
João M. Bezerra <marcospb19@hotmail.com>
2020-10-03 22:46:53 -0300

    Update docs and readme

--- README.md
+++ README.md
@@ -1,11 +1,38 @@
-# unix\_file\_permissions
+# permissions
 
-Work in progress, README will be here working soon
+This crate provide a friendly interface for permissions of the current
+process.
 
-## Acknowledgements
-Shotout do eric goes here
+`is_file_removable`, and any `is_file` function, checks if the current has
+the permissions required to conclude the operation.
 
-# TODO
-More tests
+Functions available:
+```rust
+is_file_executable
+is_file_readable
+is_file_writablea
+is_file_removable
+```
 
-Put all functions that depend on libc on a selectable feature of this crate.
+## Example:
+```rust
+use permissions::is_file_removable;
+
+use std::{fs, io, path::PathBuf};
+
+fn main() -> io::Result<()> {
+    let path = "path";
+    fs::File::create(path)?;
+
+    if is_file_removable(path)? {
+        fs::remove_file(path)?;
+    }
+
+    Ok(())
+}
+```
+
+---
+
+This crate also offers ModeBits for friendly `rwx` permissions, yet to be
+documented
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,43 @@
-//! We need more examples and documentation lmao
+//! This crate provide a friendly interface for permissions of the current
+//! process.
+//!
+//! `is_file_removable`, and any `is_file` function, checks if the current has
+//! the permissions required to conclude the operation.
+//!
+//! Functions available:
+//! ```ignore
+//! is_file_executable
+//! is_file_readable
+//! is_file_writablea
+//! is_file_removable
+//! ```
+//!
+//! # Example:
+//! ```rust
+//! use permissions::is_file_removable;
+//!
+//! use std::{fs, io, path::PathBuf};
+//!
+//! fn main() -> io::Result<()> {
+//!     let path = "path";
+//!     fs::File::create(path)?;
+//!
+//!     if is_file_removable(path)? {
+//!         fs::remove_file(path)?;
+//!     }
+//!
+//!     Ok(())
+//! }
+//! ```
+//!
+//! ---
+//!
+//! This crate also offers ModeBits for friendly `rwx` permissions, yet to be
+//! documented
 
-// Import macro from bitflags, it used to declare Classes and ModeBits.
+pub mod prelude;
+
+// Import macro from bitflags, it's used to declare Classes and ModeBits.
 #[macro_use]
 extern crate bitflags;
 
@@ -17,5 +54,3 @@ pub use functions::*;
 pub use mode_bits::ModeBits;
 /// PermissionBits...
 pub use permission_bits::PermissionBits;
-
-// TODO: read_only stuff???
